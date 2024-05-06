42deead5f3235620fb4cc08526166bfcf1833020
Begimai <begimai@MacBook-Air-Begimai.local>
2024-05-06 11:11:55 +0600

    gitignore

--- lesson/views.py
+++ lesson/views.py
@@ -1,3 +1,4 @@
+from typing import Any
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, DeleteView
 
@@ -12,4 +13,12 @@ class LessonView(ListView):
 class LessonDetailView(DetailView):
     model = Lesson
     template_name = 'lesson-detail.html'
-    context_object_name = 'content'
+    context_object_name = 'lesson'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        lesson = self.get_object()
+        tests = lesson.tests.all()
+        context['tests'] = tests
+        return context
+    
