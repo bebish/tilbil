e4482234d299c3bc57b99fd8e32e9590e3a6befc
Bekmyrza01 <bekmyrzasaliuulu01@gamil.com>
2024-05-28 16:48:24 +0600

    profil

--- Makefile
+++ Makefile
@@ -2,4 +2,4 @@ migrate:
 	python3 manage.py makemigrations
 	python3 manage.py migrate
 run:
-	python3 manage.py runserver	
+	python3 manage.py runserver
--- account/views.py
+++ account/views.py
@@ -4,7 +4,11 @@ from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from django.contrib.auth.views import LoginView
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, authenticate
 
+
+from .forms import ProfileEditForm
 from .forms import RegistrationForm
 from .models import User
 
@@ -15,9 +19,33 @@ class RegistrationView(SuccessMessageMixin,CreateView):
     success_url = reverse_lazy('lesson-view')
     success_message = 'Successfully registered'
 
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        username = form.cleaned_data['username']
+        raw_password = form.cleaned_data['password']
+        user = authenticate(username=username, password=raw_password)
+        if user is not None:
+            login(self.request, user)
+        return response
+
 class SignInView(LoginView):
     template_name = 'account/login.html'
 
+
+def profile(request):
+    return render(request, 'account/profile.html')
 def logout_view(request):
     logout(request)
     return redirect('/lesson')
+
+
+@login_required
+def profile_edit(request):
+    if request.method == 'POST':
+        form = ProfileEditForm(request.POST, request.FILES, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')  # Redirect to the profile page after saving changes
+    else:
+        form = ProfileEditForm(instance=request.user)
+    return render(request, 'account/profile-edit.html', {'form': form})
